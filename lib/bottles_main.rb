9e056f99e019d516c862a40004647bdf13a6c951
msolorio <msolorio.github@gmail.com>
2023-09-21 09:22:53 -0700

    Creates bottle number class

--- lib/bottles_main.rb
+++ lib/bottles_main.rb
@@ -17,6 +17,34 @@ class Bottles
   private
 
   def container(number)
+    BottleNumber.new(number).container
+  end
+
+  def pronoun(number)
+    BottleNumber.new(number).pronoun
+  end
+
+  def quantity(number)
+    BottleNumber.new(number).quantity
+  end
+
+  def action(number)
+    BottleNumber.new(number).action
+  end
+
+  def successor(number)
+    BottleNumber.new(number).successor
+  end
+end
+
+class BottleNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def container
     if number == 1
       "bottle"
     else
@@ -24,7 +52,7 @@ class Bottles
     end
   end
 
-  def pronoun(number)
+  def pronoun
     if number == 1
       "it"
     else
@@ -32,7 +60,7 @@ class Bottles
     end
   end
 
-  def quantity(number)
+  def quantity
     if number == 0
       "no more"
     else
@@ -40,15 +68,15 @@ class Bottles
     end
   end
 
-  def action(number)
+  def action
     if number == 0
       "Go to the store and buy some more, "
     else
-      "Take #{pronoun(number)} down and pass it around, "
+      "Take #{pronoun} down and pass it around, "
     end
   end
 
-  def successor(number)
+  def successor
     if number == 0
       99
     else
