b643b4c71a96c0059e13ea5b34d320e9b442cb27
msolorio <msolorio.github@gmail.com>
2023-09-22 22:44:19 -0700

    Adds different types of registries

--- lib/bottles_main.rb
+++ lib/bottles_main.rb
@@ -42,17 +42,75 @@ end
 class BottleNumber
   attr_reader :number
 
+  # ----------------------------------------------------
+  # follows open/closed principle
+  # dynamically deriving the class name
+  # con: since class names aren't referenced directly, programmer may decide to delete a class
+  # con: uses exception for control flow
+  # def self.for(number)
+  #   begin
+  #     const_get("BottleNumber#{number}")
+  #   rescue NameError
+  #     BottleNumber
+  #   end.new(number)
+  # end
+
+  # ----------------------------------------------------
+  # maintains a registry of classes to check. Classes register themselves.
+  # Is open for extension, creates instances of classes who's names it doesn't know
+  def self.registry
+    @registry ||= [self]
+  end
+
+  def self.register(candidate)
+    registry.prepend(candidate)
+  end
+
   def self.for(number)
-    case number
-    when 0
-      BottleNumber0
-    when 1
-      BottleNumber1
-    when 6
-      BottleNumber6
-    else
-      BottleNumber
-    end.new(number)
+    registry.find { |candidate| candidate.handles?(number) }.new(number)
+  end
+
+  # ----------------------------------------------------
+  # maintains a registry of classes to check. Each class keeps track of if it should be chosen
+  # BottleNumber superclass must always be last on the list
+  # def self.registry
+  #   [
+  #     BottleNumber0,
+  #     BottleNumber1,
+  #     BottleNumber6,
+  #   ].push(BottleNumber)
+  # end
+
+  # def self.for(number)
+  #   registry.find { |candidate| candidate.handles?(number) }.new(number)
+  # end
+
+  # ----------------------------------------------------
+  # creates hash that defaults to BottleNumber for failed key lookups and creates an instance
+  # def self.for(number)
+  #   Hash.new(BottleNumber).merge(
+  #     0 => BottleNumber0,
+  #     1 => BottleNumber1,
+  #     6 => BottleNumber6,
+  #   )[number].new(number)
+  # end
+
+  # ----------------------------------------------------
+  # def self.for(number)
+  #   case number
+  #   when 0
+  #     BottleNumber0
+  #   when 1
+  #     BottleNumber1
+  #   when 6
+  #     BottleNumber6
+  #   else
+  #     BottleNumber
+  #   end.new(number)
+  # end
+
+  def self.handles?(number)
+    true
   end
 
   def initialize(number)
@@ -85,6 +143,12 @@ class BottleNumber
 end
 
 class BottleNumber0 < BottleNumber
+  BottleNumber.register(self)
+
+  def self.handles?(number)
+    number == 0
+  end
+
   def quantity
     "no more"
   end
@@ -99,6 +163,12 @@ class BottleNumber0 < BottleNumber
 end
 
 class BottleNumber1 < BottleNumber
+  BottleNumber.register(self)
+
+  def self.handles?(number)
+    number == 1
+  end
+
   def container
     "bottle"
   end
@@ -109,6 +179,12 @@ class BottleNumber1 < BottleNumber
 end
 
 class BottleNumber6 < BottleNumber
+  BottleNumber.register(self)
+
+  def self.handles?(number)
+    number == 6
+  end
+
   def quantity
     "1"
   end
